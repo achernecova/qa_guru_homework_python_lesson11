178b9bb3b481da00e22e534effcedd1ec23401a1
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-23 23:31:37 +0300

    добавила кейсы (открытие, проверка заголовка) на страницу Контакты и Кейсы

--- tests/softmg/test_main.py
+++ tests/softmg/test_main.py
@@ -1,10 +1,58 @@
+import allure
+from allure_commons.types import Severity
+
 from tests.softmg.about_as_page import AboutCompany
+from tests.softmg.cases_page import CasesPage
+from tests.softmg.contact_page import ContactPage
 from tests.softmg.header_panel import HeaderPanel
 
+@allure.tag("normal")
+@allure.severity(Severity.NORMAL)
+@allure.label("owner", "chernetsova")
+@allure.feature("Работоспособность меню")
+@allure.story("Корректный переход на страницу О Компании")
+@allure.link("https://softmg.ru/", name="Testing")
+def test_open_page_softmg_about():
 
-def test_open_page_softmg():
-    page = HeaderPanel()
-    page.open()
-    page.open_page_about_as()
-    page_about = AboutCompany()
-    page_about.data_page()
+    with allure.step("Открываем главную страницу сайта softmg"):
+        page = HeaderPanel()
+        page.open()
+    with allure.step("В меню переходим по пункту О компании"):
+        page.open_page_about_as()
+        page_about = AboutCompany()
+    with allure.step("Проверяем, что мы находимся на нужной странице - заголовок соответствует заданному"):
+        page_about.data_page()
+
+@allure.tag("normal")
+@allure.severity(Severity.NORMAL)
+@allure.label("owner", "chernetsova")
+@allure.feature("Работоспособность меню")
+@allure.story("Корректный переход на страницу Контакты")
+@allure.link("https://softmg.ru/", name="Testing")
+def test_open_page_softmg_contact():
+
+    with allure.step("Открываем главную страницу сайта softmg"):
+        page = HeaderPanel()
+        page.open()
+    with allure.step("В меню переходим по пункту Контакты"):
+        page.open_page_about_as()
+        page_contact = ContactPage()
+    with allure.step("Проверяем, что мы находимся на нужной странице - заголовок соответствует заданному"):
+        page_contact.data_page()
+
+@allure.tag("normal")
+@allure.severity(Severity.NORMAL)
+@allure.label("owner", "chernetsova")
+@allure.feature("Работоспособность меню")
+@allure.story("Корректный переход на страницу Кейсы")
+@allure.link("https://softmg.ru/", name="Testing")
+def test_open_page_softmg_contact():
+
+    with allure.step("Открываем главную страницу сайта softmg"):
+        page = HeaderPanel()
+        page.open()
+    with allure.step("В меню переходим по пункту Кейсы"):
+        page.open_page_about_as()
+        page_cases = CasesPage()
+    with allure.step("Проверяем, что мы находимся на нужной странице - заголовок соответствует заданному"):
+        page_cases.data_page()
