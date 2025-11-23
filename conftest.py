e370143ba492015500a9486176ce439231f794d4
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-24 00:42:51 +0300

    изменила conftest.py - добавла передачу драйвера в селен     browser.config.driver = driver

--- conftest.py
+++ conftest.py
@@ -1,28 +1,34 @@
 import pytest
 from selene import browser
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from utils import attach
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope="function")
 def setup_browser():
-    options = ChromeOptions()
-    options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", "128.0")
-    options.add_argument("--window-size=1920,1080")
-    options.set_capability("selenoid:options", {
+    chrome_options = Options()
+    chrome_options.set_capability("browserName", "chrome")
+    chrome_options.set_capability("browserVersion", "128.0")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.set_capability("selenoid:options", {
         "enableVNC": True,
         "enableVideo": True
     })
+    driver = webdriver.Chrome(options=chrome_options)
+    # Передаем драйвер в Selene
+    browser.config.driver = driver
 
     browser.config.driver_remote_url = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
-    browser.config.driver_options = options
+    browser.config.driver_options = chrome_options
     browser.config.timeout = 6
 
     yield
 
     browser.quit()
     # аттачи
-    # attach.add_screenshot(browser)
-    # attach.add_logs(browser)
-    # # attach.add_video(browser)
-    # attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    # attach.add_video(browser)
+    attach.add_html(browser)
