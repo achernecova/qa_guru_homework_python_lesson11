d71c3ec1f1cf1d2713895befa29c4609d03d0df7
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-24 02:05:15 +0300

    вернула driver_remote_url как был. надо разобраться как работает подставновка.

--- conftest.py
+++ conftest.py
@@ -26,7 +26,7 @@ def setup_browser():
     })
     options.set_capability("goog:loggingPrefs", {'browser': 'ALL'})  # Включаем логирование браузера
 
-    browser.config.driver_remote_url = f'{selenoid_url}'
+    browser.config.driver_remote_url = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
     browser.config.driver_options = options
     browser.config.timeout = 6
 
