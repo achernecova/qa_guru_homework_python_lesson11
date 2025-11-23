3a2b09b87182cf48257b14a8addeb62129aaea06
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-23 21:58:36 +0300

    изменила conftest.py обратно

--- conftest.py
+++ conftest.py
@@ -1,52 +1,52 @@
-# import pytest
-#
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-#
-#
-# @pytest.fixture(scope='function')
-# def setup_browser():
-#     options = Options()
-#
-#     options.set_capability("browserName", "chrome")
-#     options.set_capability("browserVersion", "128.0")
-#     options.set_capability("selenoid:options", {
-#         "enableVNC": True,
-#         "enableVideo": True
-#     })
-#
-#     driver = webdriver.Remote(
-#         command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-#         options=options
-#     )
-#
-#     yield driver
-#
-#     driver.quit()
-
 import pytest
-from selene import browser
-from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-@pytest.fixture(scope="function", autouse=True)
+@pytest.fixture(scope='function')
 def setup_browser():
-    options = ChromeOptions()
+    options = Options()
+
     options.set_capability("browserName", "chrome")
     options.set_capability("browserVersion", "128.0")
-    options.set_capability(
-        "selenoid:options",
-        {
-            "enableVNC": True,
-            "enableVideo": True,
-            "screenResolution": "1920x1080x24",
-        },
+    options.set_capability("selenoid:options", {
+        "enableVNC": True,
+        "enableVideo": True
+    })
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
     )
 
-    browser.config.driver_remote_url = "http://localhost:4444/wd/hub"
-    browser.config.driver_options = options
-    browser.config.base_url = "https://demoqa.com"
-    browser.config.timeout = 6
+    yield driver
 
-    yield
-    browser.quit()
+    driver.quit()
+
+# import pytest
+# from selene import browser
+# from selenium.webdriver.chrome.options import Options as ChromeOptions
+#
+#
+# @pytest.fixture(scope="function", autouse=True)
+# def setup_browser():
+#     options = ChromeOptions()
+#     options.set_capability("browserName", "chrome")
+#     options.set_capability("browserVersion", "128.0")
+#     options.set_capability(
+#         "selenoid:options",
+#         {
+#             "enableVNC": True,
+#             "enableVideo": True,
+#             "screenResolution": "1920x1080x24",
+#         },
+#     )
+#
+#     browser.config.driver_remote_url = "http://localhost:4444/wd/hub"
+#     browser.config.driver_options = options
+#     browser.config.base_url = "https://demoqa.com"
+#     browser.config.timeout = 6
+#
+#     yield
+#     browser.quit()
