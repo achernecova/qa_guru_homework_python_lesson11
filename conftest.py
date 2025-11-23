66fb3dec008dc7fa74b2b24fc9721cb28d50c21e
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-24 02:03:52 +0300

    добавила файл .env Добавила парамер selenoid_url добавила зависимость python-dotenv==1.2.1

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
+import os
 
 import pytest
 from selene import browser
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-
+from dotenv import load_dotenv
 from utils import attach
 
+@pytest.fixture(scope="function", autouse=True)
+def load_env():
+    load_dotenv()
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 @pytest.fixture(scope="function", autouse=True)
 def setup_browser():
@@ -18,7 +26,7 @@ def setup_browser():
     })
     options.set_capability("goog:loggingPrefs", {'browser': 'ALL'})  # Включаем логирование браузера
 
-    browser.config.driver_remote_url = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
+    browser.config.driver_remote_url = f'{selenoid_url}'
     browser.config.driver_options = options
     browser.config.timeout = 6
 
