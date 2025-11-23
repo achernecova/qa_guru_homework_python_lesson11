a0bebc607a15d06e5a88fbf67a8aa9f0d7b1359c
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-24 01:46:43 +0300

    раскомментировала в conftest строку с прикреплением логов

--- conftest.py
+++ conftest.py
@@ -26,7 +26,7 @@ def setup_browser():
 
     attach.add_screenshot(browser)
     # TODO - не работает прикрепление логов. Ошибка в дженкинсе AttributeError: 'WebDriver' object has no attribute 'get_log'
-    # attach.add_logs(browser)
+    attach.add_logs(browser)
     attach.add_html(browser)
 
     browser.quit()
