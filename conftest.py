ed33ca3812dce0d29f0de7c8f6a83c1d5bb87518
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-24 00:54:51 +0300

    полное изменение conftest.py - ловлю ту же ошибку в дженкинсе

--- conftest.py
+++ conftest.py
@@ -1,33 +1,23 @@
+
 import pytest
 from selene import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-from utils import attach
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="function", autouse=True)
 def setup_browser():
-    chrome_options = Options()
-    chrome_options.set_capability("browserName", "chrome")
-    chrome_options.set_capability("browserVersion", "128.0")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.set_capability("selenoid:options", {
+    options = ChromeOptions()
+    options.set_capability("browserName", "chrome")
+    options.set_capability("browserVersion", "128.0")
+    options.set_capability("selenoid:options", {
         "enableVNC": True,
         "enableVideo": True
     })
 
     browser.config.driver_remote_url = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
-    browser.config.driver_options = chrome_options
+    browser.config.driver_options = options
     browser.config.timeout = 6
 
     yield
 
-    # аттачи
-    # attach.add_screenshot(browser.driver)
-    # attach.add_logs(browser.driver)
-    # # attach.add_video(browser)
-    # attach.add_html(browser.driver)
-
     browser.quit()
-
