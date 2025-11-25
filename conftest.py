b824da3adb87620d9edd483c4e589864d53fbe36
Alexandra Chernetsova <achernecova1904@gmail.com>
2025-11-25 13:35:15 +0300

    изменила немного аттачи, добавила в конфтест новую фикстуру ( с занятия 12). Пробуем запуск через дженкинс в селеноиде.

--- conftest.py
+++ conftest.py
@@ -1,40 +1,73 @@
-import os
+# import os
+#
+# import pytest
+# from selene import browser
+# from selenium.webdriver.chrome.options import Options as ChromeOptions
+# from dotenv import load_dotenv
+# from utils import attach
+
+# @pytest.fixture(scope="function", autouse=True)
+# def load_env():
+#     load_dotenv()
+#
+# selenoid_login = os.getenv("SELENOID_LOGIN")
+# selenoid_pass = os.getenv("SELENOID_PASS")
+# selenoid_url = os.getenv("SELENOID_URL")
+#
+# @pytest.fixture(scope="function", autouse=True)
+# def setup_browser():
+#     options = ChromeOptions()
+#     options.set_capability("browserName", "chrome")
+#     options.set_capability("browserVersion", "128.0")
+#     options.add_argument("--window-size=1280,900")
+#     options.set_capability("selenoid:options", {
+#         "enableVNC": True,
+#         "enableVideo": True
+#     })
+#     options.set_capability("goog:loggingPrefs", {'browser': 'ALL'})  # Включаем логирование браузера
+#
+#     browser.config.driver_remote_url = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
+#     browser.config.driver_options = options
+#     browser.config.timeout = 6
+#
+#     yield
+#     browser.driver.maximize_window()
+#     attach.add_screenshot(browser)
+#     # TODO - не работает прикрепление логов. Ошибка в дженкинсе AttributeError: 'WebDriver' object has no attribute 'get_log'
+#     #attach.add_logs(browser)
+#     attach.add_html(browser)
+#     attach.add_video(browser)
+#
+#     browser.quit()
 
 import pytest
-from selene import browser
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from dotenv import load_dotenv
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selene.support.shared import browser
+
 from utils import attach
 
-@pytest.fixture(scope="function", autouse=True)
-def load_env():
-    load_dotenv()
 
-selenoid_login = os.getenv("SELENOID_LOGIN")
-selenoid_pass = os.getenv("SELENOID_PASS")
-selenoid_url = os.getenv("SELENOID_URL")
+@pytest.fixture(scope='function')
+def driver():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "128.0",
+        "selenoid:options": {
+            "enableVNX": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
 
-@pytest.fixture(scope="function", autouse=True)
-def setup_browser():
-    options = ChromeOptions()
-    options.set_capability("browserName", "chrome")
-    options.set_capability("browserVersion", "128.0")
-    options.add_argument("--window-size=1280,900")
-    options.set_capability("selenoid:options", {
-        "enableVNC": True,
-        "enableVideo": True
-    })
-    options.set_capability("goog:loggingPrefs", {'browser': 'ALL'})  # Включаем логирование браузера
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
 
-    browser.config.driver_remote_url = "https://user1:1234@selenoid.autotests.cloud/wd/hub"
-    browser.config.driver_options = options
-    browser.config.timeout = 6
-
+    browser.config.driver = driver
     yield
-    browser.driver.maximize_window()
     attach.add_screenshot(browser)
-    # TODO - не работает прикрепление логов. Ошибка в дженкинсе AttributeError: 'WebDriver' object has no attribute 'get_log'
-    #attach.add_logs(browser)
     attach.add_html(browser)
     attach.add_video(browser)
 
--- utils/attach.py
+++ utils/attach.py
@@ -9,6 +9,7 @@ def add_screenshot(browser):
 def add_logs(browser):
     log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
     allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+
     # try:
     #     logs = browser.driver.get_log('browser')
     #     text = "\n".join([f"{l['level']}: {l['message']}" for l in logs])
